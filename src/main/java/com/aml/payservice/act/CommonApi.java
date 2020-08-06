cf4946ee5342f98c5ae324730a2fdfc482cccb39
liguiqin <xinaml@qq.com>
2020-08-06 16:24:59 +0800

    first commit

--- src/main/java/com/aml/payservice/act/CommonApi.java
+++ src/main/java/com/aml/payservice/act/CommonApi.java
@@ -93,7 +93,13 @@ public class CommonApi {
             String targetPath="/py/qr/"+name;
             String logoPath="py/logo.png";
             Map<String, String> resp = wxpay.unifiedOrder(data);
-            QRcodeUtil.encode(resp.get("code_url"), 300, 300, logoPath,targetPath);
+//            QRcodeUtil.encode(resp.get("code_url"), 300, 300, logoPath,targetPath);
+            File file=new File(targetPath);
+            byte[] bytes= QRcodeUtil.encode(resp.get("code_url"), 300, 300, logoPath);
+            log.info("bytes长度:"+bytes.length);
+            FileOutputStream os =new FileOutputStream(file);
+            os.write(bytes);
+            os.close();
             log.info("qr path: "+targetPath);
             resp.put("imagePath", "http://re.luzhiai.com:8088/"+name);
             return resp;
