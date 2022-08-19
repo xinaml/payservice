38f5543ff7016c024d030a04a3c7ebbc4c95fa7d
lgq <xinaml@qq.com>
2022-08-19 21:19:32 +0800

    11111

--- src/main/java/com/aml/payservice/A.java
+++ src/main/java/com/aml/payservice/A.java
@@ -6,6 +6,6 @@ package com.aml.payservice;
  */
 public class A {
     public static void main(String[] args) {
-        System.out.println("1111111");
+        System.out.println("222222");
     }
 }
