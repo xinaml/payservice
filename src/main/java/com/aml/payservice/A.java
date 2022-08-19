87d92de25be1db8e0dc9d52041c8dc9f0250013f
lgq <xinaml@qq.com>
2022-08-19 21:20:30 +0800

    22222

--- src/main/java/com/aml/payservice/A.java
+++ src/main/java/com/aml/payservice/A.java
@@ -6,6 +6,6 @@ package com.aml.payservice;
  */
 public class A {
     public static void main(String[] args) {
-        System.out.println("222222");
+        System.out.println("22222");
     }
 }
